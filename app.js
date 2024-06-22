c6401db29a1e9fc0fb186a52f6c09f11d3e0418b
ERJACKIE <ergauravrajpurohit@gmail.com>
2024-06-22 19:45:46 +0530

    first commit

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const connectDB = require("./db/connect")
 const products_routes = require("./routes/products")
 const PORT = process.env.PORT || 5000;
 
+// Middleware to enable CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // or specify a domain instead of '*'
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Credentials', 'true'); // if credentials are needed
+// Handle preflight requests
+if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+    } else {
+    next();
+    }
+});
+
 // Middleware to serve static files (images)
 app.use('/images', express.static('images'));
 
