8bfdfd9c82f85b9e7c8e360efdba934b8e16ed71
ERJACKIE <ergauravrajpurohit@gmail.com>
2024-06-22 17:11:16 +0530

    first commit

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ require ("dotenv").config();
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect")
+const products_routes = require("./routes/products")
 const PORT = process.env.PORT || 5000;
 
-const products_routes = require("./routes/products")
+// Middleware to serve static files (images)
+app.use('/images', express.static('images'));
+
 
 app.get("/", (req, res) => {
     res.send("hi, I am live");
--- models/product.js
+++ models/product.js
@@ -3,23 +3,38 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, "Product name must be provided"],
+    },
+    details: {
+        type: String,
+        required: [true, "Product details must be provided"],
+    },
+    img:  [
+        {
+            url: { type: String, required: true },
+            alt: { type: String, default: '' } // Optional, can be adjusted based on your needs
+        }
+    ],
+    mrp: {
+        type: Number,
+        required: [true, "MRP must be provided"],
+        min: [0, "MRP must be a positive number"],
     },
     price: {
         type: Number,
-        required: [true,"price must be provided"],
+        required: [true, "Price must be provided"],
+        min: [0, "Price must be a positive number"],
     },
-    featured: {
-        type: Boolean,
-        default: false,
-    },
+    discount: { type: Number, default: 0 },
     rating: {
         type: Number,
-        required: 4.9,
+        required: [true, "Rating must be provided"],
+        min: [0, "Rating must be at least 0"],
+        max: [5, "Rating cannot be more than 5"],
     },
     createdAt: {
         type: Date,
-        default: Date.now(),
+        default: () => Date.now(),
     },
     company: {
         type: String,
